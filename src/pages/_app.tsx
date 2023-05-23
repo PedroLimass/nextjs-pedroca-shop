fa95690a0d4f59a0e721f339899b1fc642c58af9
Pedro Lima <phlimas@outlook.com>
2023-05-23 16:44:22 -0300

    Config some files and set header with pedroca logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/styles/globals'
 import type { AppProps } from 'next/app'
+import logoShop from '../assets/logoShop.svg'
+import Image from 'next/image'
+import { Container, Header } from '@/styles/pages/app'
 
+globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoShop} alt="" width={68} />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
